2c43d68fddec60def2e358933976bcb3a6fd0519
Jackson <jackson.mooring@gmail.com>
2019-05-30 08:13:33 -0500

    Make a route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express        = require('express');
 const bodyParser     = require('body-parser');
 const app            = express();
 
-
+require('./app/routes')(app, {});
 const port = 8000;
 app.listen(port, () => {
   console.log('We are live on ' + port);
