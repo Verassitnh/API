d660cb02e53fe1d0a3b905d58ab7364beed62e78
Jackson <jackson.mooring@gmail.com>
2019-05-30 08:15:03 -0500

    Finish the route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,8 @@
 const noteRoutes = require('./note_routes');
 module.exports = function(app, db) {
   noteRoutes(app, db);
-  // Other route groups could go here, in the future
+  app.post('/users', (req, res) => {
+    // You'll create your note here.
+    res.send('Hello')
+  });
 };
